eb2d828d281ad4f711ca31cf3c9e092a5074524d
Ciocoladinii <iahradam@yahoo.com>
2023-09-02 17:39:01 +0300

    1st commit, t3 stack working with material UI

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+import { api } from "~/utils/api";
+import "~/styles/globals.css";
+import { theme } from "~/styles/theme";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
